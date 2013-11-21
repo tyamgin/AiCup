6541775cf0684a57736cfc8cfe5fd97256377ad6
tyamgin <tyamgin@gmail.com>
2013-11-21 22:58:09 +0200

    Ускорение bfs-а

--- ShortestPath.cs
+++ ShortestPath.cs
@@ -11,6 +11,8 @@ namespace Com.CodeGame.CodeTroopers2013.DevKit.CSharpCgdk
     {
         private static Queue<int> q = new Queue<int>();
         private static int[,] bfsDist;
+        private static int[] clearBfsDist;
+        private static int clearBfsDistSize = 0;
 
         // TODO: чистить не весь bfsDist
         ArrayList FastBfs(int fromX, int fromY, int length, int[,] map, Point[] filled)
@@ -19,14 +21,22 @@ namespace Com.CodeGame.CodeTroopers2013.DevKit.CSharpCgdk
             q.Enqueue(fromX);
             q.Enqueue(fromY);
             if (bfsDist == null)
+            {
                 bfsDist = new int[width, height];
-            for (int i = 0; i < width; i++)
-                for (int j = 0; j < height; j++)
-                    bfsDist[i, j] = Inf;
+                clearBfsDist = new int[2 * width * height + 1];
+                for (int i = 0; i < width; i++)
+                    for (int j = 0; j < height; j++)
+                        bfsDist[i, j] = Inf;
+            }
+            for (int i = 0; i < clearBfsDistSize; i += 2)
+                bfsDist[clearBfsDist[i], clearBfsDist[i + 1]] = Inf;
+            clearBfsDistSize = 0;
             foreach (var p in filled)
                 map[p.X, p.Y] = 1;
             
             bfsDist[fromX, fromY] = 0;
+            clearBfsDist[clearBfsDistSize++] = fromX;
+            clearBfsDist[clearBfsDistSize++] = fromY;
             var result = new ArrayList();
             while (q.Count != 0)
             {
@@ -42,6 +52,8 @@ namespace Com.CodeGame.CodeTroopers2013.DevKit.CSharpCgdk
                         if (nX >= 0 && nY >= 0 && nX < width && nY < height && map[nX, nY] == 0 && bfsDist[nX, nY] == Inf)
                         {
                             bfsDist[nX, nY] = bfsDist[x, y] + 1;
+                            clearBfsDist[clearBfsDistSize++] = nX;
+                            clearBfsDist[clearBfsDistSize++] = nY;
                             q.Enqueue(nX);
                             q.Enqueue(nY);
                         }
