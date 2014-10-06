170304dda855a4fbb14f8189dbae3cce2955e0df
tyamgin <tyamgin@gmail.com>
2014-10-06 18:10:26 +0400

    .

---
+++ CodeHockey/computeScore.js
@@ -0,0 +1,35 @@
+var username = 'tyamgin';
+var games = 50;
+var pages = 3;
+
+var gamesCnt = 0;
+var mySum = 0
+
+var calculate = function(page) {
+	if (page > pages) {
+		var totalSum = gamesCnt * 3;
+		console.log(mySum + ' / ' + totalSum);
+		return;
+	}
+
+	$.get('http://russianaicup.ru/profile/tyamgin/ownGames/page/' + page, function(html) {
+	  var doc = $(html);
+	  doc.find('.gamesTable tbody tr').each(function() {
+		  var tr = $(this);
+		  var a = tr.find('td[style="padding-left: 0; padding-right: 0;"] a[title]');
+		  var myPos = -1;
+		  for(var i = 0; i < a.length; i++)
+			 if (a.eq(i).find('span').html() === username)
+				 myPos = i;
+		  var r = tr.find('td.right:first div').eq(myPos);
+		  if (gamesCnt < games && r.html() !== null) {
+			 var myPts = parseInt(r.html());
+			 mySum += myPts;
+			 gamesCnt++;
+		  }
+	  });
+	  calculate(page + 1);
+	});
+};
+
+calculate(1);
