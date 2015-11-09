f80b8ddd4c998bde025c7ce0f7d270c8d87c4c30
tyamgin <tyamgin@gmail.com>
2015-11-09 20:35:11 +0300

    local-runner

--- CodeRacing/MyStrategy.cs
+++ CodeRacing/MyStrategy.cs
@@ -1,14 +1,18 @@
 using System;
 using Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk.Model;
 
-namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk {
-    public sealed class MyStrategy : IStrategy {
-        public void Move(Car self, World world, Game game, Move move) {
+namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk 
+{
+    public partial class MyStrategy : IStrategy 
+    {
+        public void Move(Car self, World world, Game game, Move move) 
+        {
             move.EnginePower = 1.0D;
             move.IsThrowProjectile = true;
             move.IsSpillOil = true;
 
-            if (world.Tick > game.InitialFreezeDurationTicks) {
+            if (world.Tick > game.InitialFreezeDurationTicks) 
+            {
                 move.IsUseNitro = true;
             }
         }
