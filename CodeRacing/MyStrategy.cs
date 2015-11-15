b9a1d1a1e29101e66cbbf6dbfd65a5166d75c2e3
tyamgin <tyamgin@gmail.com>
2015-11-15 11:06:15 +0300

    .

--- CodeRacing/MyStrategy.cs
+++ CodeRacing/MyStrategy.cs
@@ -98,7 +98,7 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
                 for (var i = 0; i < Opponents.Length; i++)
                 {
                     OpponentsCars[t][i] = OpponentsCars[t - 1][i].Clone();
-                    _applyMove(ways[i], OpponentsCars[t][i]);
+                    _simulateOpponentMove(ways[i], OpponentsCars[t][i]);
 #if DEBUG
                     segs[i].Add(new Point(OpponentsCars[t][i]));
 #endif
@@ -294,55 +294,7 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
             });
         }
 
-        private static double _approxRad = 500;//??
-
-        private void _applyMove(Points pts, ACar car)
-        {
-            if (pts.Count == 0)
-            {
-                Log("need more points!");
-                return;
-            }
-            var p = pts[0];
-            if (car.GetDistanceTo2(p) < _approxRad*_approxRad)
-            {
-                pts.RemoveAt(0);
-                _applyMove(pts, car);
-                return;
-            }
-            var turn = car.GetAngleTo(p);
-            var power = 1.0;
-            if (Math.Abs(turn) > 0.5)
-                power = 0.5;
-            if (Math.Abs(turn) > 1)
-                power = 0.3;
-
-            ModelMove(car, new AMove { EnginePower = power, IsBrake = false, WheelTurn = TurnRound(turn) }, simpleMode: true);
-        }
-
-        public bool CheckUseOil()
-        {
-            if (self.RemainingOilCooldownTicks != 0 || world.Tick < game.InitialFreezeDurationTicks)
-                return false;
-
-            var slick = GetOilSlick(new ACar(self));
-            var rad = slick.Radius*0.8;
-            var result = false;
-
-            for (var t = 0; t < OpponentsTicksPrediction; t++)
-            {
-                for (var i = 0; i < Opponents.Length; i++)
-                {
-                    if (slick.GetDistanceTo2(OpponentsCars[t][i]) < rad*rad
-                        && Math.Abs(OpponentsCars[t][i].WheelTurn) > 0.2)
-                    {
-                        result = true;
-                    }
-                }
-            }
-            return result;
-        }
-
+       
         private void CarMoveFunc(ACar _model, int from, int to, int step, AMove m, int time, CarCallback callback)
         {
             m.Times = 0;
---
+++ CodeRacing/OilProjectileStrategy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
+{
+    public partial class MyStrategy
+    {
+        private void _simulateOpponentMove(Points pts, ACar car)
+        {
+            if (pts.Count == 0)
+            {
+                Log("need more points!");
+                return;
+            }
+            var p = pts[0];
+            if (car.GetDistanceTo2(p) < 500 * 500)
+            {
+                pts.RemoveAt(0);
+                _simulateOpponentMove(pts, car);
+                return;
+            }
+            var turn = car.GetAngleTo(p);
+            var power = 1.0;
+            if (Math.Abs(turn) > 0.5)
+                power = 0.5;
+            if (Math.Abs(turn) > 1)
+                power = 0.3;
+
+            ModelMove(car, new AMove { EnginePower = power, IsBrake = false, WheelTurn = TurnRound(turn) }, simpleMode: true);
+        }
+
+        public bool CheckUseOil()
+        {
+            if (self.RemainingOilCooldownTicks != 0 || world.Tick < game.InitialFreezeDurationTicks)
+                return false;
+
+            var slick = GetOilSlick(new ACar(self));
+            var rad = slick.Radius * 0.8;
+            var result = false;
+
+            for (var t = 0; t < OpponentsTicksPrediction; t++)
+            {
+                for (var i = 0; i < Opponents.Length; i++)
+                {
+                    if (slick.GetDistanceTo2(OpponentsCars[t][i]) < rad * rad
+                        && Math.Abs(OpponentsCars[t][i].WheelTurn) > 0.2)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
