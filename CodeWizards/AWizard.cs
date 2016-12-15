b5c0ad063601f2ea75bd557eaaa4f9a3cb657639
tyamgin <tyamgin@gmail.com>
2016-12-15 18:58:39 +0300

    .

--- CodeWizards/AWizard.cs
+++ CodeWizards/AWizard.cs
@@ -201,7 +201,7 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
 
         public bool CheckProjectileCantDodge(AProjectile proj, ACombatUnit opp)
         {
-            return (opp is AWizard ? Utility.Range(-Math.PI/2, Math.PI/2, 4) : new[] {0.0}).All(changeAngle =>
+            return (opp is AWizard ? Utility.Range(-Math.PI/2, Math.PI/2, 6) : new[] {0.0}).All(changeAngle =>
             {
                 var path = proj.Emulate(opp, changeAngle);
                 return path.Any(x => x.State == AProjectile.ProjectilePathState.Shot && x.Target.Faction != Faction);
