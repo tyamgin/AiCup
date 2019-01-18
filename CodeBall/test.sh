1aa66fb4c965a9a5ae2528f390a5c24778f047b0
Ivan Tyamgin <tyamgin@gmail.com>
2019-01-18 21:51:46 +0300

    .

--- CodeBall/test.sh
+++ CodeBall/test.sh
@@ -19,6 +19,6 @@ rm local_runner/$ts.txt
 echo "LR started"
 sleep 2
 ./cmake-build-release/CodeBall 127.0.0.1 31002 0000000000000000 &
-./release/m36 127.0.0.1 31003 0000000000000000 > /dev/null
+./release/m37 127.0.0.1 31003 0000000000000000 > /dev/null
 
 cat local_runner/$ts.txt
