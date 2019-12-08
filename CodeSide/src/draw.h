9b6ed5abf926bbd77e3d9c4e5d7d0c84d49c599b
Ivan Tyamgin <tyamgin@gmail.com>
2019-12-08 14:41:24 +0300

    .

--- CodeSide/src/draw.h
+++ CodeSide/src/draw.h
@@ -13,6 +13,7 @@ public:
     static std::shared_ptr<Debug> debug;
 
     void drawGrid() {
+#ifdef DEBUG
 #if M_DRAW_GRID
         for (int i = 0; i < TLevel::width; i++) {
             float x = i;
@@ -25,15 +26,19 @@ public:
             debug->draw(CustomData::PlacedText(std::to_string(j), Vec2Float{0.3f, y + 0.5f}, TextAlignment::CENTER, 15, ColorFloat(0, 1, 0, 1)));
         }
 #endif
+#endif
     }
 
     void debugPoint(const TPoint& point) {
+#ifdef DEBUG
         debug->draw(CustomData::Rect(Vec2Float{float(point.x), float(point.y)},
                                      Vec2Float{0.05, 0.05},
                                      ColorFloat(1, 0, 0, 1)));
+#endif
     }
 
     void drawPath(const std::vector<TPoint>& path, ColorFloat color = ColorFloat(0, 1, 0, 1)) {
+#ifdef DEBUG
         for (int i = 1; i < (int) path.size(); i++) {
             auto x1 = (float) path[i - 1].x;
             auto y1 = (float) path[i - 1].y;
@@ -41,6 +46,7 @@ public:
             auto y2 = (float) path[i].y;
             debug->draw(CustomData::Line({x1, y1}, {x2, y2}, 0.1, color));
         }
+#endif
     }
 };
 
